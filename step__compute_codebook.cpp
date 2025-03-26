46f64c94c98382fa320519761b026ca81a8cbddf
Frank Bai <frank.byx@gmail.com>
2025-03-26 16:56:36 -0400

    Fix number of codebooks computed in step 4.

--- step__compute_codebook.cpp
+++ step__compute_codebook.cpp
@@ -2,6 +2,8 @@
  * @brief The fourth step of the algorithm. See algorithm_steps.h for more information.
  */
 
+#include <algorithm>
+
 #include "algorithm_steps.h"
 #include "subspace_decomposition.h"
 
@@ -19,16 +21,28 @@ Codebook computeCodebook(const SubspaceDecomposition& decomp,
 	codebook.reserve(subspaceIds.size());
 	for (const dim_t& subspaceId : subspaceIds)
 	{
+		// For each subspace, the number of codewords is the exponential entropy of the joint partition, rounded up to
+		// the nearest power of 2 to make full use of the encoding bits, but capped by the number of partition blocks
 		Subspace dimIds = decomp.getDimIds(subspaceId);
 		Partition subspaceJointPartition = jointPartitionByIndices(partitions, dimIds);
+		id_t numCodewords = 1 << static_cast<int>(ceil(entropy(subspaceJointPartition, false)));
+		if (numCodewords > subspaceJointPartition.blockIdToVecIds.size())
+		{
+			numCodewords = static_cast<id_t>(subspaceJointPartition.blockIdToVecIds.size());
+		}
+
+		// We use the largest partition blocks as the clusters to initialize the codewords
+		std::vector<std::vector<id_t>> clusters = std::move(subspaceJointPartition.blockIdToVecIds);
+		std::sort(clusters.begin(), clusters.end(),
+				  [](const std::vector<id_t>& a, const std::vector<id_t>& b) { return a.size() > b.size(); });
 
 		std::vector<Codeword> codewords;
-		codewords.reserve(subspaceJointPartition.blockIdToVecIds.size());
-		for (const std::vector<id_t>& cluster : subspaceJointPartition.blockIdToVecIds)
+		codewords.reserve(numCodewords);
+		for (id_t i = 0; i < numCodewords; ++i)
 		{
 			Codeword codeword(dimIds.size(), 0.0f);
 
-			for (const id_t& vecId : cluster)
+			for (const id_t& vecId : clusters[i])
 			{
 				for (dim_t i = 0; i < dimIds.size(); ++i)
 				{
@@ -38,7 +52,7 @@ Codebook computeCodebook(const SubspaceDecomposition& decomp,
 
 			for (dim_t i = 0; i < dimIds.size(); ++i)
 			{
-				codeword[i] /= cluster.size();
+				codeword[i] /= clusters[i].size();
 			}
 
 			codewords.push_back(std::move(codeword));
